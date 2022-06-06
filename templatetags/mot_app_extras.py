afffe4bc676c0b308a53af2c846e93aeca220b4b
madolphe <madolphe@ensc.Fr>
2022-06-06 21:38:08 +0200

    Add new tag to have a default language code, if en or fr are not used

--- templatetags/mot_app_extras.py
+++ templatetags/mot_app_extras.py
@@ -2,7 +2,15 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def addstr(arg1, arg2):
     """concatenate arg1 & arg2"""
     return str(arg1) + str(arg2)
+
+
+@register.simple_tag
+def check_language_code(code="None"):
+    if code != "fr" and code != "en":
+        return "en"
+    return code
