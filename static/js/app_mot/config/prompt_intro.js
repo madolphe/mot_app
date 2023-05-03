d7fb391f68d1f292768df98f6e3fe0858bb2f738
Maxime ADOLPHE <madolphe@ensc.fr>
2023-05-03 23:25:08 +0200

    add english translation to mot introduction

--- static/js/app_mot/config/prompt_intro.js
+++ static/js/app_mot/config/prompt_intro.js
@@ -21,21 +21,21 @@ if(language_code==="fr"){
     }
 }else{
     dict_intro = {
-        "cine_part1_map_1": "Thème cinéma. Séance du jour : la science fiction. \n Votre mission: retrouver les robots qui contrôlent la navette spatiale.",
-        "cine_part1_map_2": "Thème cinéma. Séance du jour: le thriller. \n Votre mission: retrouver le réalisateur du film.",
-        "cine_part1_map_3": "Thème cinéma. Séance du jour : la comédie musicale. \n Votre mission : suivre les pas de danse.",
-        "cine_part1_map_4": "Thème cinéma. Séance du jour : le western. \n Votre mission: retrouver les gourdes d’eau !",
-        "cine_part2_map_1": "Thème cinéma. Séance du jour : les gladiateurs. \n Votre mission: de retrouver l’épée du gladiateur.",
-        "cine_part2_map_2": "Thème cinéma. Séance du jour : le crime. \n Votre mission: retrouver les empreintes du criminel.",
-        "cine_part2_map_3": "Thème cinéma. Séance du jour : l’espionnage. \n Votre mission: retrouver les pistolets.",
-        "cine_part2_map_4": "Thème cinéma. Séance du jour : les gangsters. \n Votre mission: retrouver les diamants.",
-        "goblin_map_1": "Vous êtes un garde. Débarrasser vous des des gobelins. \n Attention, ils vous tendent un piège dans la forêt...",
-        "goblin_map_2": "Vous êtes un garde. Débarrasser vous des des gobelins. \n Attention, ils vous tendent un piège dans le chateau...",
-        "goblin_map_3": "Vous êtes un garde. Débarrasser vous des des gobelins. \n Attention, ils vous tendent un piège dans le désert...",
-        "goblin_map_4": "Vous êtes un garde. Débarrasser vous des des gobelins. \n Attention, ils vous tendent un piège dans la montagne...",
-        "mythology_map_1": "Vous incarnez un aventurier. Votre mission: retrouver toutes les amulettes. \n Retrouvez les dans le temple aztèque.",
-        "mythology_map_2": "Vous incarnez un aventurier. Votre mission: retrouver toutes les amulettes. \n Retrouvez les dans le chateau.",
-        "mythology_map_3": "Vous incarnez un aventurier. Votre mission: retrouver toutes les amulettes. \n Retrouvez les dans le temple antique.",
-        "mythology_map_4": "Vous incarnez un aventurier. Votre mission: retrouver toutes les amulettes. \n Retrouvez les dans la pyramide."
+        "cine_part1_map_1": "Theme: Cinema. Today's screening: Science Fiction.\n Your mission: Find the robots that control the spaceship.",
+        "cine_part1_map_2": "Theme: Cinema. Today's screening: Thriller. \n Your mission: Find the director of the film.",
+        "cine_part1_map_3": "Theme: Cinema. Today's screening: Musical comedy. \n Your mission: Follow the dance steps.",
+        "cine_part1_map_4": "Theme: Cinema. Today's screening: Western. \n Your mission: Find the water gourds!",
+        "cine_part2_map_1": "Theme: Cinema. Today's screening: Gladiators. \n Your mission: Find the gladiator's sword.",
+        "cine_part2_map_2": "Theme: Cinema. Today's screening: Crime. \n Your mission: Find the criminal's fingerprints.",
+        "cine_part2_map_3": "Theme: Cinema. Today's screening: Espionage. \n Your mission: Find the pistols.",
+        "cine_part2_map_4": "Theme: Cinema. Today's screening: Gangsters. \n Your mission: Find the diamonds.",
+        "goblin_map_1": "You are a guard. Get rid of the goblins. \n Be careful, they set a trap for you in the forest...",
+        "goblin_map_2": "You are a guard. Get rid of the goblins. \n Be careful, they set a trap for you in the castle...",
+        "goblin_map_3": "You are a guard. Get rid of the goblins. \n Be careful, they set a trap for you in the desert...",
+        "goblin_map_4": "You are a guard. Get rid of the goblins. \n Be careful, they set a trap for you in the mountains...",
+        "mythology_map_1": "You play as an adventurer. Your mission: find all the amulets. \n Find them in the Aztec temple.",
+        "mythology_map_2": "You play as an adventurer. Your mission: find all the amulets. \n Find them in the castle.",
+        "mythology_map_3": "You play as an adventurer. Your mission: find all the amulets. \n Find them in the ancient temple.",
+        "mythology_map_4": "You play as an adventurer. Your mission: find all the amulets. \n Find them in the pyramid."
     }
 }
