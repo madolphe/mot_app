b98d28655614f2067b0af8b5cf8fffd52c65d5f7
Maxime ADOLPHE <madolphe@ensc.fr>
2023-09-04 22:53:03 +0200

    change admin.py to add secondary task and update fields to display for episode table

--- admin.py
+++ admin.py
@@ -9,6 +9,7 @@ class ExportCsvMixin:
     Export model as csv
     (Snapshot found in https://readthedocs.org/projects/django-admin-cookbook/downloads/pdf/latest/)
     """
+
     def export_as_csv(self, request, queryset):
         meta = self.model._meta
         field_names = [field.name for field in meta.fields]
@@ -19,13 +20,25 @@ class ExportCsvMixin:
         for obj in queryset:
             row = writer.writerow([getattr(obj, field) for field in field_names])
         return response
+
     export_as_csv.short_description = "Export Selected"
 
 
 class EpisodeAdmin(admin.ModelAdmin, ExportCsvMixin):
-    list_display = ('__str__', 'participant', 'get_results', 'n_targets', 'n_distractors', 'probe_time',
-                    'tracking_time', 'speed_max', 'radius', 'idle_time', 'is_training')
+    list_display = (
+    '__str__', 'participant', 'get_results', 'n_targets', 'nb_target_retrieved', 'nb_distract_retrieved',
+    'n_distractors', 'get_F1_score',
+    'probe_time',
+    'tracking_time', 'speed_max', 'radius', 'idle_time', 'is_training')
     list_filter = ['participant']
+    actions = ["export_as_csv"]
+
+
+class SecondaryAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = (
+        'participant', 'episode', 'type', 'nbanners', 'delta_orientation',
+        'response_window', 'answers', 'start_times', 'RTs', 'episode_date', 'episode_time')
+    # list_filter = ['participant']
     actions = ["export_as_csv"]
 
 
@@ -45,3 +58,4 @@ class CognitiveResultsAdmin(admin.ModelAdmin, ExportCsvMixin):
 admin.site.register(Episode, EpisodeAdmin)
 admin.site.register(CognitiveTask, CognitiveTaskAdmin)
 admin.site.register(CognitiveResult, CognitiveResultsAdmin)
+admin.site.register(SecondaryTask, SecondaryAdmin)
