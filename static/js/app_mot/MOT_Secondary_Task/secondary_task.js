3ebd133c7bf2d159b0b8923314c4105182e2f39b
Maxime ADOLPHE <madolphe@ensc.fr>
2023-09-04 23:06:58 +0200

    correct detection task to fit with guidelines

--- static/js/app_mot/MOT_Secondary_Task/secondary_task.js
+++ static/js/app_mot/MOT_Secondary_Task/secondary_task.js
@@ -1,13 +1,16 @@
 // @TODO : get keyboard press for detection
 
-class Secondary_Task{
-    constructor(image, type, SRI_max, RSI, tracking_time, delta_orientation, other_objects){
+class Secondary_Task {
+    constructor(image, type, SRI_max, response_window, tracking_time, delta_orientation, other_objects, nbanners) {
         this.image = image;
         this.type = type;
+        // number of banners to present
+        this.nbanners = nbanners;
         // duration of interval between presentation of sec task:
         this.SRI_max = SRI_max;
-        // duration of display of sec task:
-        this.RSI = RSI;
+        // duration of display of sec task is different of response window!
+        this.display_time = 500;
+        this.response_window = response_window + this.display_time;
         this.delta_orientation = delta_orientation;
         this.tracking_time = tracking_time;
         // total number of secondary task presentation:
@@ -16,37 +19,55 @@ class Secondary_Task{
         this.index_pres = 0;
         // mode
         this.display = false;
+        this.in_response_window = false;
         this.other_objects = other_objects;
         this.in_img_scaling = 0.1;
-        this.results = []
+        this.results = [];
+        this.current_banner_id = 0;
+        this.stimuli_times = [];
+        this.create_event();
     }
-    find_position(){
-        if(this.type == 'discrimination'){
+
+    create_event() {
+        this.window_size = this.available_time / this.nbanners;
+        let margin_btw_stimuli = this.window_size * 0.05;
+        let max_start_stimulus_display = this.window_size - margin_btw_stimuli - this.response_window;
+        for (let i = 0; i < this.nbanners; i++) {
+            let start = parseFloat((Math.random() * (max_start_stimulus_display - margin_btw_stimuli) + margin_btw_stimuli));
+            // stimuli_times encompasses all time event to start display a stimulus
+            this.stimuli_times.push(start);
+        }
+    }
+
+    find_position() {
+        if (this.type == 'discrimination') {
             console.log('change_or');
             this.delta_orientation = random(0, 80);
         }
         do {
             this.r = random(this.other_objects[0].area_min + this.other_objects[0].radius,
                 this.other_objects[0].area_max - this.other_objects[0].radius);
-            this.theta = random(0,2*Math.PI);
-            this.x = Math.round(this.r*Math.cos(this.theta));
-            this.y = Math.round(this.r*Math.sin(this.theta));
-        } while(!this.is_free_position())
+            this.theta = random(0, 2 * Math.PI);
+            this.x = Math.round(this.r * Math.cos(this.theta));
+            this.y = Math.round(this.r * Math.sin(this.theta));
+        } while (!this.is_free_position())
     }
-    is_free_position(){
+
+    is_free_position() {
         // check for free location considering other objects
-        this.other_objects.forEach(function(item){
+        this.other_objects.forEach(function (item) {
             if (Math.sqrt((this.x - item.pos.x) ** 2 + (this.y - item.pos.y) ** 2) < item.radius) {
                 return false;
             }
         }, this);
         return true
     }
-    display_task(){
-            if(this.display){
+
+    display_task() {
+        if (this.display) {
             push();
             imageMode(CENTER);
-            translate(this.x + windowWidth/2, this.y + windowHeight/2);
+            translate(this.x + windowWidth / 2, this.y + windowHeight / 2);
             scale(0.15);
             image(this.image, 0, 0);
             pop();
@@ -54,26 +75,26 @@ class Secondary_Task{
             push();
             stroke('gold');
             strokeWeight(30);
-            translate(this.x+ windowWidth/2,  this.y + windowHeight/2);
+            translate(this.x + windowWidth / 2, this.y + windowHeight / 2);
             scale(this.in_img_scaling);
-            line(0,- this.image.height/2, 0, this.image.height/2);
+            line(0, -this.image.height / 2, 0, this.image.height / 2);
             pop();
             // space btween branch is set to 1/6 of the image
-            let space = this.in_img_scaling*this.image.height/6;
-            let hypo = (this.in_img_scaling*this.image.width/4)*(1/Math.cos(radians(this.delta_orientation)));
-            let opp = hypo*(Math.sin(radians(this.delta_orientation)));
-            for(let i=0; i<5; i++){
-                if((2-i)*space - opp < -this.in_img_scaling*this.image.height/2){
-                    opp = -( -this.in_img_scaling*this.image.height/2 - (2-i)*space );
-                    hypo = opp/(Math.sin(radians(this.delta_orientation)));
-                }else{
-                    hypo = (this.in_img_scaling*this.image.width/4)*(1/Math.cos(radians(this.delta_orientation)));
-                    opp = hypo*(Math.sin(radians(this.delta_orientation)));
+            let space = this.in_img_scaling * this.image.height / 6;
+            let hypo = (this.in_img_scaling * this.image.width / 4) * (1 / Math.cos(radians(this.delta_orientation)));
+            let opp = hypo * (Math.sin(radians(this.delta_orientation)));
+            for (let i = 0; i < 5; i++) {
+                if ((2 - i) * space - opp < -this.in_img_scaling * this.image.height / 2) {
+                    opp = -(-this.in_img_scaling * this.image.height / 2 - (2 - i) * space);
+                    hypo = opp / (Math.sin(radians(this.delta_orientation)));
+                } else {
+                    hypo = (this.in_img_scaling * this.image.width / 4) * (1 / Math.cos(radians(this.delta_orientation)));
+                    opp = hypo * (Math.sin(radians(this.delta_orientation)));
                 }
                 push();
                 stroke('gold');
                 strokeWeight(2);
-                translate(this.x+ windowWidth/2,  this.y + windowHeight/2 + (2-i)*space);
+                translate(this.x + windowWidth / 2, this.y + windowHeight / 2 + (2 - i) * space);
                 rotate(-radians(this.delta_orientation));
                 // scale(0.07);
                 line(0, 0, hypo, 0);
@@ -82,7 +103,7 @@ class Secondary_Task{
                 push();
                 stroke('gold');
                 strokeWeight(2);
-                translate(this.x+ windowWidth/2,  this.y + windowHeight/2 + (2-i)*space);
+                translate(this.x + windowWidth / 2, this.y + windowHeight / 2 + (2 - i) * space);
                 rotate(radians(this.delta_orientation));
                 // scale(0.07);
                 line(-hypo, 0, 0, 0);
@@ -90,80 +111,135 @@ class Secondary_Task{
             }
         }
     }
-    timer_display(){
+
+    launch_sequence_of_windows_timer() {
+        this.timer_pause();
+        // start first window
+        setTimeout(this.update_index_window.bind(this), this.window_size);
+    }
+
+    timer_display() {
         this.time_start_display = Date.now();
-        this.timer_disp_id = setTimeout(this.start_pause.bind(this), this.RSI);
+        // start display during response_window mseconds
+        // If timer_disp is finished, just stop to display stimulus
+        this.timer_disp_id = setTimeout(this.stop_display_stimulus.bind(this), this.display_time);
+        // If timer_response finished, store results
+        this.timer_response_window = setTimeout(this.pause_till_end_window.bind(this), this.response_window);
     }
-    timer_pause(){
-        setTimeout(this.start_display.bind(this), this.SRI_max);
+
+    pause_till_end_window() {
+        // Push results when no click:
+        this.save_last_trial(0);
+        this.in_response_window = false;
     }
-    keyboard_pressed(key_value){
+    stop_display_stimulus(){
+        // This method is called to stop displaying stimulus
+        // After display_time (500ms) or if participant has clicked
+        this.display = false;
+    }
+
+    timer_pause() {
+        setTimeout(this.start_display.bind(this), this.stimuli_times[this.current_banner_id]);
+    }
+
+    update_index_window() {
+        this.current_time = Date.now();
+        // Method called when window time is over
+        // Pass to next window banner:
+        this.current_banner_id++;
+        if (this.current_banner_id < this.nbanners) {
+            // launch a new timer for window if there is still banners to present
+            setTimeout(this.update_index_window.bind(this), this.window_size);
+            // launch a new timer for pause before start displaying the stimulus
+            this.timer_pause();
+        }
+    }
+
+    keyboard_pressed(key_value) {
         // if keyboard_press and the banner is displayed ->
-        if(this.display){
-            if(this.type=="detection"){
-                if(key_value==32){
-                this.display = false;
-                this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 1]);
-                clearTimeout(this.timer_disp_id);
-                this.available_time -= (Date.now() - this.time_start_display);
-                this.timer_pause();
-                }
-            }else if (this.type=="discrimination"){
-                // if key == 'S':
-                if(key_value==83){
-                    this.display = false;
-                    if(this.delta_orientation>30){
-                        // wrong key pressed:
-                        this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 0]);
-                    }else{
-                        // Correct key pressed:
-                         this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 1]);
-                    }
+        if (this.in_response_window) {
+            if (this.type == "detection") {
+                if (key_value == 32) {
+                    // Participant has answered, stop response window and display timer
                     clearTimeout(this.timer_disp_id);
-                    this.available_time -= (Date.now() - this.time_start_display);
-                    this.timer_pause();
-                // else if key == 'f':
-                }else if (key_value==70){
+                    clearTimeout(this.timer_response_window)
+                    correct_sound.play();
+                    // store results
+                    this.save_last_trial(1)
+                    // this.display might already been set to false
                     this.display = false;
-                    if(this.delta_orientation>30){
-                        // correct key pressed:
-                        this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 0]);
-                    }else{
+                    this.in_response_window = false;
+                }
+            } else if (this.type == "discrimination") {
+                // DISCRIMININATION IS NOT WORKING !!
+                // if key == 'S':
+                if (key_value == 83) {
+                    this.display = false;
+                    if (this.delta_orientation > 30) {
                         // wrong key pressed:
+                        error_sound.play();
+                        this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 0]);
+                    } else {
+                        // Correct key pressed:
+                        correct_sound.play();
                         this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 1]);
                     }
                     clearTimeout(this.timer_disp_id);
                     this.available_time -= (Date.now() - this.time_start_display);
                     this.timer_pause();
+                    // else if key == 'f':
+                } else if (key_value == 70) {
+                    this.display = false;
+                    if (this.delta_orientation > 30) {
+                        // correct key pressed:
+                        correct_sound.play();
+                        this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 0]);
+                    } else {
+                        // wrong key pressed:
+                        error_sound.play();
+                        this.results.push([this.delta_orientation, Date.now() - this.time_start_display, 1]);
+                    }
+                    clearTimeout(this.timer_disp_id);
+                    this.pause_till_end_window();
                 }
             }
-        }else{
-            // otherwise, the user press without asking for it:
-            this.results.push([0, 0, 0]);
         }
     }
-    start_display(){
-        // function used after SRI_max:
-        this.available_time -= this.SRI_max;
+
+    save_last_trial(ans) {
+        // For detection: ans can be 1 (hit correct), 0 (no hit)
+        // For discrimination: ans can be 1 (hit correct), 0 (hit false), -1 (no hit)
+        let RT = Date.now() - this.time_start_display;
+        this.results.push([this.nbanners,
+            this.response_window,
+            this.delta_orientation,
+            this.stimuli_times,
+            RT,
+            ans]);
+    }
+
+    start_display() {
         // if available time left:
-        if(this.available_time-this.RSI>0){
-                this.find_position();
-                this.display=true;
-                // launch timer for display:
-                this.timer_display();
-        }
-    }
-    start_pause(){
-        // function used to start pause
-        if(!this.display){
-            // user has found object so it's not displayed anymore:
-            console.log("problem, timer hasn't been really reset")
-        }else{
-            this.results.push([this.delta_orientation, this.RSI, 0]);
-            this.display = false;
-            this.available_time -= this.RSI;
-            this.timer_pause();
-        }
+        this.find_position();
+        this.display = true;
+        this.in_response_window = true;
+        // launch timer for display:
+        this.timer_display();
     }
 }
+
+
+// start_pause() {
+//     // function used to start pause
+//     if (!this.display) {
+//         // user has found object so it's not displayed anymore:
+//         console.log("problem, timer hasn't been really reset")
+//     } else {
+//         this.results.push([this.delta_orientation, this.response_window, 0]);
+//         this.display = false;
+//         this.available_time -= this.response_window;
+//         this.timer_pause();
+//     }
+// }
+
 
